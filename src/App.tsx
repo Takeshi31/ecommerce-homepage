4ad8b8740df4af88d022df433108134665e4900f
takeshi31 <heber87@gmail.com>
2023-04-14 16:02:45 -0500

    feat: Agregar configuración TypeScript, dependencias de React y configuración base

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+function App() {
+  return (
+    <main>
+      <header>
+      </header>
+      <section>
+        <h1>Sección</h1>
+      </section>
+      <footer>
+        <p>Derechos reservados © 2023</p>
+      </footer>
+    </main>
+  );
+}
+
+export default App
